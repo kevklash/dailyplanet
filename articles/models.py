5ea0693ca2913b8e580d28a3ccb05a9aaa767206
Kevin <kevkde@gmail.com>
2021-04-09 11:53:54 -0600

    Created 'Journalist' and 'Article' models. Migration done. Inluded .gitignore file.

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Journalist(models.Model):
+	jouralist_name = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.jouralist_name
+
+
+class Article(models.Model):
+	date = models.DateField()
+	headline = models.CharField(max_length=200)
+	content = models.TextField()
+	jouralist_name = models.ForeignKey(Journalist, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.headline
